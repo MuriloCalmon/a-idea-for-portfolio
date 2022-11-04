df021d6021cb03d034c65aa337eedd3baf74f365
Murilo Calmon <murilocalmon80@gmail.com>
2022-11-04 13:04:27 -0300

    first update mobile

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+
 var typed = new Typed('.dev-text', {
   strings: ['Dev front end', 'Analista de sistemas', 'Tec. em TI'],
   typeSpeed: 100,
@@ -5,29 +7,41 @@ var typed = new Typed('.dev-text', {
   loop: true
 })
 
-const categories = document.querySelector('.categories')
+function fillSkillsAccordingToLevel() {
 
-const n = (categories.childElementCount)
+  const skills = document.querySelector('.skills')
+  const numberOfChildrenOfDivSkills = (skills.childElementCount)
+  const childrenInSkills = skills.children
 
-
-for (let i = 0; i < n; i++) {
-  if (i % 2 == 0) {
-    if (categories.children[i].innerText === 'HTML') {
-      document.querySelector('.div-html-color').style.width = '60%'
-    } else if (categories.children[i].innerText === 'CSS') {
-      document.querySelector('.div-css-color').style.width = '60%'
-    } else if (categories.children[i].innerText === 'Java Script') {
-      document.querySelector('.div-js-color').style.width = '40%'
-    } else if (categories.children[i].innerText === 'Node js') {
-      document.querySelector('.div-nd-color').style.width = '20%'
-    } else if (categories.children[i].innerText === 'Type Script') {
-      document.querySelector('.div-ts-color').style.width = '20%'
-    } else if (categories.children[i].innerText === 'React js') {
-      document.querySelector('.div-rs-color').style.width = '20%'
-    } else if (categories.children[i].innerText === 'Inglês') {
-      document.querySelector('.div-in-color').style.width = '15%'
+  for (let children = 0; children < numberOfChildrenOfDivSkills; children++) {
+    if (children % 2 == 0) {
+      if (childrenInSkills[children].innerText === 'HTML') {
+        document.querySelector('.div-html-color').style.width = '60%'
+      } else if (childrenInSkills[children].innerText === 'CSS') {
+        document.querySelector('.div-css-color').style.width = '60%'
+      } else if (childrenInSkills[children].innerText === 'Java Script') {
+        document.querySelector('.div-js-color').style.width = '40%'
+      } else if (childrenInSkills[children].innerText === 'Node js') {
+        document.querySelector('.div-nd-color').style.width = '20%'
+      } else if (childrenInSkills[children].innerText === 'Type Script') {
+        document.querySelector('.div-ts-color').style.width = '20%'
+      } else if (childrenInSkills[children].innerText === 'React js') {
+        document.querySelector('.div-rs-color').style.width = '20%'
+      } else if (childrenInSkills[children].innerText === 'Inglês') {
+        document.querySelector('.div-in-color').style.width = '15%'
+      }
     }
   }
 }
+
+const menuMobile = document.querySelector('#menu-mobile')
+
+function handleToggleMenu() {
+  const nav = document.querySelector("#navigation").classList.toggle('active')
+}
+
+menuMobile.addEventListener("click", handleToggleMenu)
+
+fillSkillsAccordingToLevel()
 
 
